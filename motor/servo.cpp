abf7188a0e1d5b1835819c394102d8595214e9b4
lhh8048 <lhh8048@naver.com>
2017-07-26 00:26:15 +0900

    Add files via upload

---
+++ motor/servo.cpp
@@ -0,0 +1,68 @@
+/*
+ * The MIT License (MIT)
+Copyright (c) 2015 Jetsonhacks
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+// servoExample.cpp
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <termios.h>
+#include <time.h>
+#include <JHPWMPCA9685.h>
+
+
+// Calibrated for a Robot Geek RGS-13 Servo
+// Make sure these are appropriate for the servo being used!
+
+// int servoMin = 120 ;
+// int servoMax = 720 ;
+
+int getkey() {
+    int character;
+    struct termios orig_term_attr;
+    struct termios new_term_attr;
+
+    /* set the terminal to raw mode */
+    tcgetattr(fileno(stdin), &orig_term_attr);
+    memcpy(&new_term_attr, &orig_term_attr, sizeof(struct termios));
+    new_term_attr.c_lflag &= ~(ECHO|ICANON);
+    new_term_attr.c_cc[VTIME] = 0;
+    new_term_attr.c_cc[VMIN] = 0;
+    tcsetattr(fileno(stdin), TCSANOW, &new_term_attr);
+
+    /* read a character from the stdin stream without blocking */
+    /*   returns EOF (-1) if no character is available */
+    character = fgetc(stdin);
+
+    /* restore the original terminal attributes */
+    tcsetattr(fileno(stdin), TCSANOW, &orig_term_attr);
+
+    return character;
+}
+
+// Map an integer from one coordinate system to another
+// This is used to map the servo values to degrees
+// e.g. map(90,0,180,servoMin, servoMax)
+// Maps 90 degrees to the servo value
+
+int map ( int x, int in_min, int in_max, int out_min, int out_max) {
+    int toReturn =  (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min ;
+    // For debugging:
+    // printf("MAPPED %d to: %d\n", x, toReturn);
+    return toReturn ;
+}
