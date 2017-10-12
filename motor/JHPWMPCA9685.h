dbbc26211f9237a853e5d765acfe71fdf904b1a2
lhh8048 <lhh8048@naver.com>
2017-10-12 11:47:48 +0900

    Add files via upload

--- motor/JHPWMPCA9685.h
+++ motor/JHPWMPCA9685.h
@@ -1,14 +1,18 @@
 /*
  * The MIT License (MIT)
+
 Copyright (c) 2015 Jetsonhacks
+
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
 in the Software without restriction, including without limitation the rights
 to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 copies of the Software, and to permit persons to whom the Software is
 furnished to do so, subject to the following conditions:
+
 The above copyright notice and this permission notice shall be included in all
 copies or substantial portions of the Software.
+
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -37,7 +41,7 @@ public:
     int kI2CFileDescriptor ;        // File Descriptor to the PCA9685
     int kI2CAddress ;               // Address of PCA9685; defaults to 0x40
     int error ;
-    PCA9685(int address=0x40);
+    PCA9685(int address=0x70);
     ~PCA9685() ;
     bool openPCA9685() ;
     void closePCA9685();
